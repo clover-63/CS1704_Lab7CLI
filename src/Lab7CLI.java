0e9ea90cbfd65b0ebc681ac9657ee7e3d791d0a4
clover-63 <2335768@brunel.ac.uk>
2026-02-16 23:48:55 +0000

    testing things

--- src/Lab7CLI.java
+++ src/Lab7CLI.java
@@ -1,7 +1,11 @@
 
 public class Lab7CLI {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		System.out.println("Starting Program...");
+		
+		Thread.sleep(1000);
+		
+		System.out.println("Program has Ended.");
 	}
 }
